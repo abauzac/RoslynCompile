4b6856265c05434e678927cc89e244516756dc51
amzac <amaury.bauzac@gmail.com>
2017-01-08 18:27:28 +0100

    First commit'

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System.Runtime.Loader;
+
+namespace RoslynCompileSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(@"
+                using System;
+
+                namespace RoslynCompileSample
+                {
+                    public class Writer
+                    {
+                        public void Write(string message)
+                        {
+                            Console.WriteLine(message);
+                        }
+                    }
+                }");
+
+            string assemblyName = Path.GetRandomFileName();
+            MetadataReference[] references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).GetTypeInfo().Assembly.Location)
+            };
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic => 
+                        diagnostic.IsWarningAsError || 
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                    
+                    Type type = assembly.GetType("RoslynCompileSample.Writer");
+                    object obj = Activator.CreateInstance(type);
+                    type.GetTypeInfo().GetMethod("Write")
+                      .Invoke(obj, new object[] { "Hello World" });
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
